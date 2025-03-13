8534de4fd404d5e9c0fe552363bad9c71336c350
Ankita Kumari <163445458+Ankita-Kumari0309@users.noreply.github.com>
2025-03-13 10:35:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import string
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import word_tokenize
+
 import time
 
 # Load dependencies
